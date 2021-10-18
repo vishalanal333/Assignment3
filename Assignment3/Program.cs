f16303d2a3b160c4e944cf1ebcf00993444f26bb
Vishal Anal <vishalkumaranal@forcebolt.com>
2021-10-18 16:15:20 +0530

    Implementaion of roman to number

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -4,29 +4,80 @@ namespace Assignment3
 {
     class Program
     {
-        static String intToRoman(int num)
+        public  int value(char rom )
         {
-            String[] m = { "", "M", "MM", "MMM" };
-            String[] c = { "",  "C",  "CC",  "CCC",  "CD",
-                       "D", "DC", "DCC", "DCCC", "CM" };
-            String[] x = { "",  "X",  "XX",  "XXX",  "XL",
-                       "L", "LX", "LXX", "LXXX", "XC" };
-            String[] i = { "",  "I",  "II",  "III",  "IV",
-                       "V", "VI", "VII", "VIII", "IX" };
+            if (rom == 'I')
+                return 1;
+            if (rom == 'V')
+                return 5;
+            if (rom == 'X')
+                return 10;
+            if (rom == 'L')
+                return 50;
+            if (rom == 'C')
+                return 100;
+            if (rom == 'D')
+                return 500;
+            if (rom == 'M')
+                return 1000;
+            else
+                return -1;
+        }
+        public int RomToInt(string str)
+        {
+            int ans = 0;
+            for (int i = 0; i < str.Length; i++)
+            {              
+                int s1 = value(str[i]);
+               
+                if (i+1< str.Length)
+                {
+                    int s2 = value(str[i + 1]);
+                    int check = s2 - s1;
+                    if (s1 < s2)
+                    {
+                        if (check==4 ||check== 40 || check==90 || check==400 || check==900)
+                        {
+                            ans += s2 - s1;
+                            i++;
+                        }
+                        else 
+                        {
+                            
+                            Console.WriteLine("invalid input");
+                            ans = 0;
+                            break;
 
-            String thousands = m[num / 1000];
-            String hundereds = c[(num % 1000) / 100];
-            String tens = x[(num % 100) / 10];
-            String ones = i[num % 10];
+                        }
+                    }
+                    else if(i != 0&&value(str[i - 1]) < s1 )
+                    {
+                        Console.WriteLine("invalid input");
+                        ans = 0;
+                        break;
+                    }
+                    else
+                    {
+                        ans += s1;
+                    }
+                }
+                else
+                {
+                    ans += s1;
 
-            String ans = thousands + hundereds + tens + ones;
-
+                }
+                
+            }
             return ans;
+            
         }
         public static void Main()
         {
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(intToRoman(number));
+            Program obj = new Program();
+            string str = Console.ReadLine();
+            Console.WriteLine(obj.RomToInt(str));
+
         }
     }
 }
+// xxx
